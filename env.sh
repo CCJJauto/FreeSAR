ae65cc817743d203ba80ff36866e493466e887e3
chao.zhou2 <chao.zhou2@nio.com>
2022-12-17 22:00:36 +0800

    Update the env for FreeSAR build

--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-MAZU_DEV_IMAGE=ubuntu
+MAZU_DEV_IMAGE=zhouchao0429/freesar_env_ubuntu:v0.1
 LOCAL_FREESAR_PATH=`pwd`
 if ! docker pull "${MAZU_DEV_IMAGE}"; then
     error "Failed to pull docker image ${MAZU_DEV_IMAGE}, check locally"
@@ -13,4 +13,4 @@ if ! docker pull "${MAZU_DEV_IMAGE}"; then
         info "Docker image ${MAZU_DEV_IMAGE} exist locally"
     fi
 fi
-sudo docker run -itv "$LOCAL_FREESAR_PATH":/FreeSAR ubuntu
+sudo docker run -itv "$LOCAL_FREESAR_PATH":/FreeSAR zhouchao0429/freesar_env_ubuntu:v0.1
