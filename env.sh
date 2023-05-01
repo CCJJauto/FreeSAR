ecd9278e1f52460fad6456ea314d5adc2b75ee8e
chao.zhou2 <chao.zhou2@nio.com>
2023-05-01 18:29:24 +0800

    change env.sh not to pull image each time

--- env.sh
+++ env.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 MAZU_DEV_IMAGE=zhouchao0429/freesar_env_ubuntu:v0.3
 LOCAL_FREESAR_PATH=`pwd`
-if ! docker pull "${MAZU_DEV_IMAGE}"; then
-    error "Failed to pull docker image ${MAZU_DEV_IMAGE}, check locally"
-
-       # check if the docker image exist locally
-    check_result=`docker image ls ${MAZU_DEV_IMAGE} | wc -l`
-    if [[ "$check_result" != "2" ]]; then
-        error "Docker image ${MAZU_DEV_IMAGE} does not exist locally"
+# check if the docker image exist locally
+check_result=`docker image ls ${MAZU_DEV_IMAGE} | wc -l`
+if [[ "$check_result" != "2" ]]; then
+    error "Docker image ${MAZU_DEV_IMAGE} does not exist locally"
+    if ! docker pull "${MAZU_DEV_IMAGE}"; then
+        error "Failed to pull docker image ${MAZU_DEV_IMAGE}, check locally"
         exit 1
-    else
-        info "Docker image ${MAZU_DEV_IMAGE} exist locally"
     fi
+else
+    info "Docker image ${MAZU_DEV_IMAGE} exist locally"
 fi
 sudo docker run --net host -itv "$LOCAL_FREESAR_PATH":/FreeSAR ${MAZU_DEV_IMAGE} /bin/bash
-
